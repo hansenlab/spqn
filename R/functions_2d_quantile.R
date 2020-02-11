a55ef46e0aa8ea415e826ff929b98512ea6f86f7
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-02-11 15:11:11 -0500

    Update functions_2d_quantile.R

--- R/functions_2d_quantile.R
+++ R/functions_2d_quantile.R
@@ -60,7 +60,10 @@ create_bins <- function(n_genes, n_bins = 20, size_bins = 400) {
 
 get_grps<-function(avelog2cpm,ngrp=20,size_grp=400){
   ngene=length(avelog2cpm) 
-  
+  grp_label=cut(1:(ngene-size_grp+1),ngrp-1)
+  grp_loc0=split(1:(ngene-size_grp+1),grp_label)
+  grp_loc=list()
+
   if(size_grp-length(grp_loc0[[1]])<5){
     grp_label=cut(1:ngene,ngrp)
     grp_loc0=split(1:ngene,grp_label)
@@ -70,7 +73,6 @@ get_grps<-function(avelog2cpm,ngrp=20,size_grp=400){
   }else{
     grp_label=cut(1:(ngene-size_grp+1),ngrp-1)
     grp_loc0=split(1:(ngene-size_grp+1),grp_label)
-    grp_loc=list()
     for(i in 1:(ngrp-1)){
       grp_loc[[i]]=c(grp_loc0[[i]][1]:(grp_loc0[[i]][1]+size_grp-1))
     }
