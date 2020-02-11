bdd628cb3360d04d43800492ea9f708c1da3a4b3
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-02-11 13:00:18 -0500

    Update functions_2d_quantile.R

--- R/functions_2d_quantile.R
+++ R/functions_2d_quantile.R
@@ -58,19 +58,25 @@ create_bins <- function(n_genes, n_bins = 20, size_bins = 400) {
 
 
 
-get_grps <- function(cor_mat, ngrp=20, size_grp=400){
-    ngene <- nrow(cor_mat)
-    
-    grp_label <- cut(1:(ngene-size_grp+1), ngrp-1)
-    grp_loc0 <- split(1:(ngene-size_grp+1), grp_label)
-    
-    grp_loc <- list()
-    for(i in 1:(ngrp-1)){
-        grp_loc[[i]] <- c(grp_loc0[[i]][1]:(grp_loc0[[i]][1]+size_grp-1))
+get_grps<-function(avelog2cpm,ngrp=20,size_grp=400){
+  ngene=length(avelog2cpm) 
+  
+  if(size_grp-length(grp_loc0[[1]])<5){
+    grp_label=cut(1:ngene,ngrp)
+    grp_loc0=split(1:ngene,grp_label)
+    for(i in 1:(ngrp)){
+      grp_loc[[i]]=grp_loc0[[i]]
     }
-    grp_loc[[ngrp]] <- c((ngene-size_grp+1):ngene)
-    
-    grp_loc
+  }else{
+    grp_label=cut(1:(ngene-size_grp+1),ngrp-1)
+    grp_loc0=split(1:(ngene-size_grp+1),grp_label)
+    grp_loc=list()
+    for(i in 1:(ngrp-1)){
+      grp_loc[[i]]=c(grp_loc0[[i]][1]:(grp_loc0[[i]][1]+size_grp-1))
+    }
+    grp_loc[[ngrp]]=c((ngene-size_grp+1):ngene)
+  }
+  grp_loc
 }
 
 
