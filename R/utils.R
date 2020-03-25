eb946fbcfe45f555bb26ecddefaadfec43f629a4
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-03-25 15:12:49 -0400

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,22 +1,7 @@
-.get_grps <- function(cor_mat, ngrp=20, size_grp=400){
-    ngene <- nrow(cor_mat)
-    grp_label <- cut(seq_len(ngene-size_grp+1), ngrp-1)
-    grp_loc0 <- split(seq_len(ngene-size_grp+1), grp_label)
-    grp_loc <- list()
-
-    if(size_grp-length(grp_loc0[[1]])<5){
-        grp_label <- cut(seq_len(ngene), ngrp)
-        grp_loc0 <- split(seq_len(ngene), grp_label)
-        for(i in seq_len(ngrp)){
-            grp_loc[[i]] <- grp_loc0[[i]]
-        }
-    }else{
-        grp_label <- cut(seq_len(ngene-size_grp+1), ngrp-1)
-        grp_loc0 <- split(seq_len(ngene-size_grp+1), grp_label)
-        for(i in seq_len(ngrp-1)){
-            grp_loc[[i]] <- c(grp_loc0[[i]][1]:(grp_loc0[[i]][1]+size_grp-1))
-        }
-        grp_loc[[ngrp]] <- c((ngene-size_grp+1):ngene)
-    }
-    grp_loc
+.digestMatrix <- function(mat, digits = 6) {
+    content <- sprintf(paste0("%.", digits, "f"), mat)
+    # NOTE: Handling signed zero as per IEEE specs
+    zero <- paste(c("0.", rep("0", digits)), collapse = "")
+    content[content == paste0("-", zero)] <- zero
+    digest::digest(c(content, rownames(mat), colnames(mat)))
 }
