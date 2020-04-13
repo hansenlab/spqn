e621002cc6f682642a4f5434e13203e71ced204f
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-12 22:52:46 -0400

    Update spqn.R

--- R/spqn.R
+++ R/spqn.R
@@ -7,9 +7,12 @@
     if(size_grp-length(grp_loc0[[1]])<5){
         grp_label <- cut(seq_len(ngene), ngrp)
         grp_loc0 <- split(seq_len(ngene), grp_label)
-        for(i in seq_len(ngrp)){
-            grp_loc[[i]] <- grp_loc0[[i]]
-        }
+        grp_loc = grp_loc0
+        # for(i in seq_len(ngrp)){
+            # grp_loc[[i]] <- grp_loc0[[i]]
+            # e.g. idx <- seq_len(ngrp)
+            # e.g. grp_loc[idx] = grp_loc0[idx]
+        # }
     }else{
         grp_label <- cut(seq_len(ngene-size_grp+1), ngrp-1)
         grp_loc0 <- split(seq_len(ngene-size_grp+1), grp_label)
