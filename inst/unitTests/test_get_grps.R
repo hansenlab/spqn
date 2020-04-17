90c29d5da2befbc5d11566532e848eefbe1583e2
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-16 21:48:33 -0400

    Update test_get_grps.R

--- inst/unitTests/test_get_grps.R
+++ inst/unitTests/test_get_grps.R
@@ -1,6 +1,6 @@
 test_get_grps <- function(){
   data(grp_loc_target)
-  load(file.path(path.package("spqn"), "useful", "grp_loc_target.rda"))
+  load(file.path(path.package("spqn"), "unitTests", "grp_loc_target.rda"))
 
   grp_loc_assigend = get_grp_loc(cor_mat=array(dim=c(100,100)),ngrp=10)
   grp_loc_assigend =  matrix(unlist(grp_loc_assigend), ncol = 10, byrow = TRUE)
