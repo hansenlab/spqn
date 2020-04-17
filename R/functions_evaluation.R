01104a68c80ee3f433bd6c92ac6afe1fade58a1a
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-17 13:17:27 -0400

    Update functions_evaluation.R

--- R/functions_evaluation.R
+++ R/functions_evaluation.R
@@ -52,100 +52,102 @@ qqplot_condition_exp <- function(cor_mat, ave_exp, i, j){
 
 
 plot_signal_condition_exp <- function(cor_mat, ave_exp, signal) { 
-    idx <- order(ave_exp)
-    cor_mat <- cor_mat[idx, idx]
-    if(isTRUE(all.equal(signal, 0))){
-        ngrp <- 10
-        ngene <- ncol(cor_mat)
-        idx <- seq_len(ngene)
-        grp_label <- cut(idx, ngrp)
-        grp_loc <- split(idx, grp_label) 
-        
-        length_group <- lapply(grp_loc, length)
-        length_group <- length_group*(length_group-1)/2
-        length_group_cumulate <- cumsum(length_group)
-
-        cor_vec_all <- data.frame(matrix(ncol = 2, nrow = length_group_cumulate[10]))
-        colnames(cor_vec_all) <- c("correlation",  "group")
-                
-        for(i in seq_len(10)) {
-            cor_tmp <- cor_mat[grp_loc[[i]], grp_loc[[i]]]
-            cor_tmp <- cor_tmp[upper.tri(cor_tmp)]
-            cor_tmp <- as.numeric(cor_tmp)
-            if(i==1){
-                idx1 <- 1
-            }else{
-                idx1 <- length_group_cumulate[i-1]+1
-            }
-            idx2 <- length_group_cumulate[i]
-            idx <- c(idx1 : idx2)
-            cor_vec_all$correlation[idx] <- cor_tmp
-            length_tmp <- length_group[i]
-            cor_vec_all$group[idx] <- rep(i,length_tmp)
-        }
-        cor_vec_all <- data.frame(cor_vec_all)
-        names(cor_vec_all) <- c("correlation", "group")
-        cor_vec_all$group <- as.factor(cor_vec_all$group)
-        ggplot(cor_vec_all, aes_string(x = "correlation", y = "group")) +
-            geom_density_ridges2(fill="blue") +
-            theme_ridges( grid = TRUE) + theme(axis.title.x = element_blank()) + 
-            geom_vline(xintercept = 0, linetype="dotted", color = "black", size=0.3)
-    } else {
-        ncor <- length(which(upper.tri(cor_mat)))
-        nsignal <- ncor*signal
-        nsignal_grp <- nsignal/100
-
-        # list_cor_sig <- list_cor_back <- grp_back <- grp_sig <- c()
-        grp_locs <- get_grp_loc(cor_mat)
-        list_cor_sig <- grp_sig <- numeric(10*nsignal_grp)
-        nbackground_group <- lapply(grp_locs, length)
-        nbackground_group <- nbackground_group*(nbackground_group-1)/2
-        list_cor_back_cumulate <- cumsum(list_cor_back)
-        list_cor_back <- grp_back <- numeric(list_cor_back_cumulate[10])
-        for(ngrp in seq_len(10)) {
-            loc_back <- grp_locs[[ngrp]]
-            cor_back <- cor_mat[loc_back,loc_back]
-            cor_back <- cor_back[upper.tri(cor_back)]
-            order_cor_ori_grp <- order(abs(cor_back), decreasing=TRUE)
-            idx <- seq_len(nsignal_grp)
-            id_signal <- order_cor_ori_grp[idx]
-            cor_signal_ori <- cor_back[id_signal]
-            
-            idx1 <- (i-1)*nsignal_grp+1
-            idx2 <- i*nsignal_grp
-            idx <- c(idx1 : idx2)
-            list_cor_sig[idx] <- cor_signal_ori
-            grp_sig[idx] <- rep(ngrp, length(cor_signal_ori)
-
-            if(i==1){
-                idx <- seq_len(list_cor_back_cumulate[i])
-                list_cor_back[idx] <- cor_back
-                grp_back[idx] <- rep(ngrp, length(cor_back))
-            }else{
-                idx1 <- list_cor_back_cumulate[i-1]+1)
-                idx2 <- list_cor_back_cumulate[i]
-                idx <- c(idx1:idx2)
-                list_cor_back[idx] <- cor_back
-                grp_back[idx] <- rep(ngrp, nbackground_group[i])
-            }                                                                                                          
-        }
-        df_sig <- data.frame(correlation=list_cor_sig, bin=grp_sig, group="signal")
-        df_back <- data.frame(correlation=list_cor_back, bin=grp_back, group="background")
-        df_merge <- rbind(df_sig,df_back)
-        df_merge$bin_group  <-  paste(df_merge$bin, df_merge$group)
-        ggplot(df_merge, aes_string(y = "bin")) +
-            geom_density_ridges(
-                aes_string(x = "correlation", fill = "bin_group"),
-                alpha = .8, color = "white") +
-            labs(x = "correlation", y = "bin")  +
-            scale_y_discrete(limits=c(1:10)) +
-            scale_fill_cyclical(
-                labels = c("background","signal"),
-                values = c("#0000ff", "#ff0000"),
-                name = "group", guide = "legend") +
-            theme_ridges(grid = FALSE)
+  idx <- order(ave_exp)
+  cor_mat <- cor_mat[idx, idx]
+  if(isTRUE(all.equal(signal, 0))){
+    ngrp <- 10
+    ngene <- ncol(cor_mat)
+    idx <- seq_len(ngene)
+    grp_label <- cut(idx, ngrp)
+    grp_loc <- split(idx, grp_label) 
+    
+    length_group <- lapply(grp_loc, length)
+    length_group <- length_group*(length_group-1)/2
+    length_group_cumulate <- cumsum(length_group)
+    
+    cor_vec_all <- data.frame(matrix(ncol = 2, nrow = length_group_cumulate[10]))
+    colnames(cor_vec_all) <- c("correlation",  "group")
+    
+    for(i in seq_len(10)) {
+      cor_tmp <- cor_mat[grp_loc[[i]], grp_loc[[i]]]
+      cor_tmp <- cor_tmp[upper.tri(cor_tmp)]
+      cor_tmp <- as.numeric(cor_tmp)
+      if(i==1){
+        idx1 <- 1
+      }else{
+        idx1 <- length_group_cumulate[i-1]+1
+      }
+      idx2 <- length_group_cumulate[i]
+      idx <- c(idx1 : idx2)
+      cor_vec_all$correlation[idx] <- cor_tmp
+      length_tmp <- length_group[i]
+      cor_vec_all$group[idx] <- rep(i,length_tmp)
     }
+    cor_vec_all <- data.frame(cor_vec_all)
+    names(cor_vec_all) <- c("correlation", "group")
+    cor_vec_all$group <- as.factor(cor_vec_all$group)
+    ggplot(cor_vec_all, aes_string(x = "correlation", y = "group")) +
+      geom_density_ridges2(fill="blue") +
+      theme_ridges( grid = TRUE) + theme(axis.title.x = element_blank()) + 
+      geom_vline(xintercept = 0, linetype="dotted", color = "black", size=0.3)
+    }else{
+    ncor <- length(which(upper.tri(cor_mat)))
+    nsignal <- ncor*signal
+    nsignal_grp <- nsignal/100
+    
+    # list_cor_sig <- list_cor_back <- grp_back <- grp_sig <- c()
+    grp_locs <- get_grp_loc(cor_mat)
+    list_cor_sig <- grp_sig <- numeric(10*nsignal_grp)
+    nbackground_group <- lapply(grp_locs, length)
+    nbackground_group <- unlist(nbackground_group)
+    nbackground_group <- nbackground_group*(nbackground_group-1)/2
+    list_cor_back_cumulate <- cumsum(nbackground_group)
+    list_cor_back <- grp_back <- numeric(list_cor_back_cumulate[10])
+    for(ngrp in seq_len(10)) {
+      loc_back <- grp_locs[[ngrp]]
+      cor_back <- cor_mat[loc_back,loc_back]
+      cor_back <- cor_back[upper.tri(cor_back)]
+      order_cor_ori_grp <- order(abs(cor_back), decreasing=TRUE)
+      idx <- seq_len(nsignal_grp)
+      id_signal <- order_cor_ori_grp[idx]
+      cor_signal_ori <- cor_back[id_signal]
+      
+      idx1 <- (ngrp-1)*nsignal_grp+1
+      idx2 <- ngrp*nsignal_grp
+      idx <- c(idx1 : idx2)
+      list_cor_sig[idx] <- cor_signal_ori
+      grp_sig[idx] <- rep(ngrp, length(cor_signal_ori))
+                          
+      if(ngrp==1){
+          idx <- seq_len(list_cor_back_cumulate[ngrp])
+          list_cor_back[idx] <- cor_back
+          grp_back[idx] <- rep(ngrp, length(cor_back))
+      }else{
+      idx1 <- list_cor_back_cumulate[ngrp-1]+1
+      idx2 <- list_cor_back_cumulate[ngrp]
+      idx <- c(idx1:idx2)
+      list_cor_back[idx] <- cor_back
+      grp_back[idx] <- rep(ngrp, nbackground_group[ngrp])
+                          }                                                                                                          
+    }
+    df_sig <- data.frame(correlation=list_cor_sig, bin=grp_sig, group="signal")
+    df_back <- data.frame(correlation=list_cor_back, bin=grp_back, group="background")
+    df_merge <- rbind(df_sig,df_back)
+    df_merge$bin_group  <-  paste(df_merge$bin, df_merge$group)
+    ggplot(df_merge, aes_string(y = "bin")) +
+      geom_density_ridges(
+        aes_string(x = "correlation", fill = "bin_group"),
+        alpha = .8, color = "white") +
+      labs(x = "correlation", y = "bin")  +
+      scale_y_discrete(limits=c(1:10)) +
+      scale_fill_cyclical(
+        labels = c("background","signal"),
+        values = c("#0000ff", "#ff0000"),
+        name = "group", guide = "legend") +
+      theme_ridges(grid = FALSE)
+  }
 }
+
 
 plot_IQR_condition_exp <- function(IQR_list){
     IQR_cor_mat <- IQR_list$IQR_cor_mat
