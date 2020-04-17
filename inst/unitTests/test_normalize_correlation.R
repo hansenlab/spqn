ad2ee9272b5cba168501404cac66d7146b2dc7b6
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-16 21:48:42 -0400

    Update test_normalize_correlation.R

--- inst/unitTests/test_normalize_correlation.R
+++ inst/unitTests/test_normalize_correlation.R
@@ -1,6 +1,6 @@
 test_normalize_correlation <- function(){
   data(gtex.4k)
-  load(file.path(path.package("spqn"), "useful", "grp_loc_target.rda"))
+  load(file.path(path.package("spqn"), "unitTests", "grp_loc_target.rda"))
   cor_m <- cor(t(assay(gtex.4k)))
   ave_logrpkm <- rowData(gtex.4k)$ave_logrpkm
   cor_m_spqn_test <- normalize_correlation(cor_m, ave_exp=ave_logrpkm, ngrp=20, size_grp=300, ref_grp=18)
