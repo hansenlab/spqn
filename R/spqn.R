ecb994b307179822845fb4e7f01fb95e69dbc1ae
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-17 11:26:17 -0400

    Update spqn.R

--- R/spqn.R
+++ R/spqn.R
@@ -1,15 +1,19 @@
 .get_grps <- function(cor_mat, ngrp=20, size_grp=400){
     ngene <- nrow(cor_mat)
-    grp_label <- cut(seq_len(ngene-size_grp+1), ngrp-1)
-    grp_loc0 <- split(seq_len(ngene-size_grp+1), grp_label)
-    grp_loc <- list()
-
+    #idx <- seq_len(ngene-size_grp+1)
+    #grp_label <- cut(idx, ngrp-1)
+    #grp_loc0 <- split(idx, grp_label)
+    #grp_loc <- list()
+    
     if(size_grp-length(grp_loc0[[1]])<5){
-        grp_label <- cut(seq_len(ngene), ngrp)
-        grp_loc <- split(seq_len(ngene), grp_label)
+        idx <- seq_len(ngene)
+        grp_label <- cut(idx, ngrp)
+        grp_loc <- split(idx, grp_label)
     }else{
-        grp_label <- cut(seq_len(ngene-size_grp+1), ngrp-1)
-        grp_loc0 <- split(seq_len(ngene-size_grp+1), grp_label)
+        grp_loc <- vector(mode = "list", length = ngrp)
+        idx = seq_len(ngene-size_grp+1)
+        grp_label <- cut(idx, ngrp-1)
+        grp_loc0 <- split(idx, grp_label)
         for(i in seq_len(ngrp-1)){
             grp_loc[[i]] <- c(grp_loc0[[i]][1]:(grp_loc0[[i]][1]+size_grp-1))
         }
