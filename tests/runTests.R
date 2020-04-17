b1e5cb22feef5b7369c3f0d59ccd052482a62a9a
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-16 21:40:49 -0400

    Update runTests.R

--- tests/runTests.R
+++ tests/runTests.R
@@ -1,2 +1,2 @@
-require("spqn","spqnData","SummarizedExperiment") || stop("unable to load the required packages")
+require("spqn") || stop("unable to load the required packages")
 BiocGenerics:::testPackage("spqn")
