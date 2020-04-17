e0b2104f865d366550b86029d042dfc53e56ee05
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-17 15:02:36 -0400

    Delete utils.R

--- R/utils.R
+++
@@ -1,7 +0,0 @@
-.digestMatrix <- function(mat, digits = 6) {
-    content <- sprintf(paste0("%.", digits, "f"), mat)
-    # NOTE: Handling signed zero as per IEEE specs
-    zero <- paste(c("0.", rep("0", digits)), collapse = "")
-    content[content == paste0("-", zero)] <- zero
-    digest::digest(c(content, rownames(mat), colnames(mat)))
-}
