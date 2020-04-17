aa1330ac8ab5d2a39611978aead7084b47982274
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-16 22:10:40 -0400

    Update test_normalize_correlation.R

--- inst/unitTests/test_normalize_correlation.R
+++ inst/unitTests/test_normalize_correlation.R
@@ -1,11 +1,12 @@
 test_normalize_correlation <- function(){
   load(file.path(path.package("spqn"), "unitTests", "ave_logrpkm.rda"))
   load(file.path(path.package("spqn"), "unitTests", "exp_mat.rda"))
-
+  load(file.path(path.package("spqn"), "unitTests", "cor_m_spqn_hash.rda"))
+  
   cor_m <- cor(t(exp_mat))
 
-  cor_m_spqn <- normalize_correlation(cor_m, ave_exp=ave_logrpkm, ngrp=20, size_grp=300, ref_grp=18)
-  checkEquals(cor_m_spqn_hash, .digest.matrix(cor_m_spqn))
+  cor_m_spqn_test <- normalize_correlation(cor_m, ave_exp=ave_logrpkm, ngrp=20, size_grp=300, ref_grp=18)
+  checkEquals(cor_m_spqn_hash, .digest.matrix(cor_m_spqn_test))
 }
 
  
