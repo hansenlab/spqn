302dc30ffb7197459f41c24ba485391bf26a1edd
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-17 15:01:50 -0400

    Update test_normalize_correlation.R

--- inst/unitTests/test_normalize_correlation.R
+++ inst/unitTests/test_normalize_correlation.R
@@ -1,16 +1,14 @@
 test_normalize_correlation <- function(){
-  # load(file.path(path.package("spqn"), "unitTests", "ave_logrpkm.rda"))
-  # load(file.path(path.package("spqn"), "unitTests", "exp_mat.rda"))
-  load(file.path(path.package("spqn"), "unitTests", "cor_m_spqn_hash.rda"))
+  load(file.path(path.package("spqn"), "unitTests", "cor_m_spqn_sample.rda"))
   data(gtex.4k)
-  # exp_mat=assay(gtex.4k)
   cor_m <- cor(t(assay(gtex.4k)))
 
-  # cor_m <- cor(t(exp_mat))
   ave_logrpkm <- rowData(gtex.4k)$ave_logrpkm
 
   cor_m_spqn_test <- normalize_correlation(cor_m, ave_exp=ave_logrpkm, ngrp=20, size_grp=300, ref_grp=18)
-  checkEquals(cor_m_spqn_hash, spqn:::.digestMatrix(cor_m_spqn_test))
+  idx <- seq_len(500)
+  cor_m_spqn_test_sample <- cor_m_spqn_test[idx, idx]
+  checkEquals(cor_m_spqn_sample, cor_m_spqn_test_sample)
 }
 
  
