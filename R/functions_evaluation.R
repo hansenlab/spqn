04cdde4b618e9369d75c7a64689efd11047b0b3f
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-17 12:55:59 -0400

    Update functions_evaluation.R

--- R/functions_evaluation.R
+++ R/functions_evaluation.R
@@ -100,8 +100,8 @@ plot_signal_condition_exp <- function(cor_mat, ave_exp, signal) {
         list_cor_sig <- grp_sig <- numeric(10*nsignal_grp)
         nbackground_group <- lapply(grp_locs, length)
         nbackground_group <- nbackground_group*(nbackground_group-1)/2
-        list_cor_back <- grp_back <- numeric(sum(nbackground_group))
         list_cor_back_cumulate <- cumsum(list_cor_back)
+        list_cor_back <- grp_back <- numeric(list_cor_back_cumulate[10])
         for(ngrp in seq_len(10)) {
             loc_back <- grp_locs[[ngrp]]
             cor_back <- cor_mat[loc_back,loc_back]
