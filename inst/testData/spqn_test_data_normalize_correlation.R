451b4d8cf3604dc03a364b4a09a6a242bcb2c745
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2020-04-17 21:54:50 -0400

    Add files via upload

---
+++ inst/testData/spqn_test_data_normalize_correlation.R
@@ -0,0 +1,18 @@
+library(spqn)
+library(spqnData)
+library(SummarizedExperiment)
+library(RUnit)
+
+dir_data = "/users/ywang/packages/data_generation/" 
+data(gtex.4k)
+cor_m <- cor(t(assay(gtex.4k)))
+
+ave_logrpkm <- rowData(gtex.4k)$ave_logrpkm
+
+cor_m_spqn <- normalize_correlation(cor_m, ave_exp=ave_logrpkm, ngrp=20, size_grp=300, ref_grp=18)
+
+cor_m_spqn_sample = cor_m_spqn[1:500,1:500]
+cor_m_spqn_sample = round(cor_m_spqn_sample,4)
+save(cor_m_spqn_sample,file=paste0(dir_data,"cor_m_spqn_sample.rda"))
+
+sessionInfo()
